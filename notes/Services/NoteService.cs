df68a67b273794670389e05378422771eccf60b4
lucosmo <lukasz.kosmaczewski@gmail.com>
2023-06-03 10:49:17 +0100

    Implemented Get and GetAll methods

--- notes/Services/NoteService.cs
+++ notes/Services/NoteService.cs
@@ -1,6 +1,42 @@
-﻿namespace notes.Services
+﻿using Microsoft.EntityFrameworkCore;
+using notes.Data;
+using notes.Models;
+
+namespace notes.Services
 {
-    public class NoteService
+    public class NoteService : INoteService
     {
+        List<Note> notes;
+        private readonly NotesContext _context;
+
+        public NoteService(NotesContext context)
+        {
+            _context = context;
+        }
+
+        public Task Add(Note note)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Note> Get(int id)
+        {
+            return await _context.Notes.FirstOrDefaultAsync(x =>  x.Id == id);
+        }
+
+        public async Task<IEnumerable<Note>> GetAll()
+        {
+            return await _context.Notes.OrderBy(x => x.DateOfEdition).ToListAsync();
+        }
+
+        public Task Update(Note note)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
