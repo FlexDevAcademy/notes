0f444408782b19cb0cd31d15dd6cd3750bb7c798
lucosmo <lukasz.kosmaczewski@gmail.com>
2023-06-03 10:33:46 +0100

    created DbContext

---
+++ notes/Data/NotesContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using notes.Models;
+
+namespace notes.Data
+{
+    public class NotesContext : DbContext
+    {
+        public NotesContext(DbContextOptions<NotesContext> options) : base(options)
+        { 
+
+        }
+
+        public DbSet<Note>Notes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
